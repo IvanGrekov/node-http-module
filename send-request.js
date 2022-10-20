b741bb3789721e082ba632c6268408e7f889665f
Ivan Grekov <ivan.grekov@windmill.digital>
2022-10-20 20:43:37 +0300

    interceptors examples

--- send-request.js
+++ send-request.js
@@ -15,6 +15,39 @@ const PORT = process.env.PORT;
 //     },
 // );
 
+// export const api = axios.create({
+//     baseURL: 'https://gorest.co.in/public-api',
+// });
+
+// const checkTokenInterceptor = (config) => {
+//     const token = localStorage.getItem('token');
+
+//     if (token) {
+//         config.headers.Authorization = `Bearer ${token}`;
+//     }
+
+//     return config;
+// };
+
+// const responseSuccessInterceptor = (response) => {
+//     const data = response.data;
+
+//     if (!data._meta.success) {
+//         return { error: data.result };
+//     }
+
+//     return { response: data };
+// };
+
+// const networkErrorInterceptor = (error) => {
+//     const responseError = error?.response?.data;
+
+//     return { error: responseError || error };
+// };
+
+// api.interceptors.request.use(checkTokenInterceptor);
+// api.interceptors.response.use(responseSuccessInterceptor, networkErrorInterceptor);
+
 axios
     .get(`http://localhost:${PORT}/index?pageNumber=1&age=20&age=21`)
     .then((res) => console.log('data', res.data))
